524b184c3ab8ecf65d394eae9d2d402fbbb24998
sudip22-p <paudelsudip134@gmail.com>
2025-04-07 23:07:53 +0545

    upd: path update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -64,7 +64,7 @@ export default function Home() {
                 Explore Posts
               </Link>
               <Link
-                href="/posts/new"
+                href="/posts"
                 className="bg-blue-500 text-white px-6 py-3 rounded-lg font-medium hover:bg-blue-400 transition-colors text-center"
               >
                 Create New Post
